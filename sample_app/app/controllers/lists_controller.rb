54a19a3bd98bfb69f7af586487d344e674638819
Uma1ra <135537802+Uma1ra@users.noreply.github.com>
2023-06-26 05:25:49 +0000

    [Start~Finish] 3-3 [index]

--- sample_app/app/controllers/lists_controller.rb
+++ sample_app/app/controllers/lists_controller.rb
@@ -1,21 +1,24 @@
 class ListsController < ApplicationController
   def new
-    
+
     @list = List.new
-    
+
   end
-  
+
   def create
-    
+
     list = List.new(list_params)
-    
+
     list.save
-    
+
     redirect_to "/top"
-    
-  end 
+
+  end
 
   def index
+    
+    @lists = List.all
+    
   end
 
   def show
@@ -23,11 +26,11 @@ class ListsController < ApplicationController
 
   def edit
   end
-  
+
   private
-  
+
   def list_params
     params.require(:list).permit(:title, :body)
   end
-  
+
 end
